12e48f95f2c8b7f4ac23ecb7ca614e431b80e5a6
Vasile <allfortrashmail@gmail.com>
2023-08-11 22:11:24 +0300

    setup app theme (#3)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social_media_with_clean_architecture/src/config/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: CustomTheme.theme(),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
