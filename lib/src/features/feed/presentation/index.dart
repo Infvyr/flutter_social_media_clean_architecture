2173bf7598089352aa9a3f193083c556fe6eadf1
Vasile <allfortrashmail@gmail.com>
2023-08-12 18:20:21 +0300

    setup app router (#4)

---
+++ lib/src/features/feed/presentation/index.dart
@@ -0,0 +1,2 @@
+export './views/feed_screen.dart';
+export './views/discover_screen.dart';
---
+++ lib/src/shared/presentation/custom_nav_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../constants/index.dart';
+
+class CustomNavBar extends StatelessWidget {
+  const CustomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.black,
+      notchMargin: 10,
+      child: SizedBox(
+        height: 75,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: () {
+                context.goNamed(AppRoutes.feed.name);
+              },
+              icon: const Icon(Icons.home),
+              iconSize: 30,
+            ),
+            IconButton(
+              onPressed: () {
+                context.goNamed(AppRoutes.discover.name);
+              },
+              icon: const Icon(Icons.search),
+              iconSize: 30,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add_circle),
+              iconSize: 30,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.message),
+              iconSize: 30,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.person),
+              iconSize: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
