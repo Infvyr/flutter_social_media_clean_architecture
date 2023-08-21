f287005396e757de9e62c7bae4e836d657e143e7
Vasile <allfortrashmail@gmail.com>
2023-08-21 18:27:55 +0300

    prepare video upload

--- lib/src/features/content/presentation/add_content_screen.dart
+++ lib/src/features/content/presentation/add_content_screen.dart
@@ -1,7 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class AddContentScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class AddContentScreen extends StatefulWidget {
   const AddContentScreen({super.key});
+
+  @override
+  State<AddContentScreen> createState() => _AddContentScreenState();
+}
+
+class _AddContentScreenState extends State<AddContentScreen> {
+  Future<void> handleVideoUpload(BuildContext context) async {
+    XFile? video = await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+    if (video == null) {
+      return;
+    }
+    final directory = await getApplicationDocumentsDirectory();
+    final fileName = basename(video.path);
+    final savedVideo = await File(video.path).copy('${directory.path}/$fileName');
+
+    if (await savedVideo.exists()) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              content: Text('Unfortunatly, this video has already been uploaded. Please select another video!'),
+            ),
+          );
+      }
+      return;
+    } else {
+      debugPrint('Video does not exist $savedVideo');
+      // TODO: upload the video
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +49,7 @@ class AddContentScreen extends StatelessWidget {
       ),
       body: Center(
         child: FilledButton(
-          onPressed: () {},
+          onPressed: () async => await handleVideoUpload(context),
           child: const Text('Select video'),
         ),
       ),
